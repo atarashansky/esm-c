fcfd69145aa081b62e860909d88943dffc70fedb
atarashansky <atarashansky@chanzuckerberg.com>
2024-12-06 09:38:06 -0800

    Update attention.py
    Signed-off-by: atarashansky <atarashansky@chanzuckerberg.com>

--- esm/layers/attention.py
+++ esm/layers/attention.py
@@ -61,7 +61,7 @@ class MultiHeadAttention(nn.Module):
         if seq_id is not None:
             # Where True, enable participation in attention.
             if not isinstance(seq_id, torch.Tensor):
-                seq_id = torch.tensor(seq_id)
+                seq_id = torch.tensor([seq_id])
                 
             mask_BLL = seq_id.unsqueeze(-1) == seq_id.unsqueeze(-2)
             mask_BHLL = mask_BLL.unsqueeze(1)
